e6c1ecf0d55d1b74235e4f76c1810fa266a3ee44
M. Rehan <mrehanlm93@gmail.com>
2023-02-13 21:39:23 +0500

    Configure setuptools

---
+++ ixdiagnose/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+VERSION = '0.1'
+
+setup(
+    name='ixdiagnose',
+    description='Generate TrueNAS Scale System Debug',
+    version=VERSION,
+    include_package_data=True,
+    packages=find_packages(),
+    license='GNU3',
+    platforms='any',
+)
