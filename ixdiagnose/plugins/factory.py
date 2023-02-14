7ad350a54250a761e3c739a4c6e1f05ca52fdb5e
M. Rehan <mrehanlm93@gmail.com>
2023-02-14 19:47:50 +0500

    Add plugin factory

--- ixdiagnose/plugins/factory.py
+++ ixdiagnose/plugins/factory.py
@@ -1,4 +1,30 @@
+import errno
+
+from ixdiagnose.exceptions import CallError
+
+from .base import Plugin
+from .hardware import Hardware
 
 
 class PluginFactory:
-    plugins = {}
+
+    def __init__(self):
+        self._creators: dict = {}
+
+    def register(self, plugin: Plugin) -> None:
+        self._creators[plugin.name] = plugin
+
+    def plugin(self, name: str) -> Plugin:
+        if name not in self._creators:
+            raise CallError(f'Unable to locate {name!r} plugin.', errno=errno.ENOENT)
+        return self._creators[name]
+
+    def get_plugins(self) -> dict:
+        return self._creators
+
+
+plugin_factory = PluginFactory()
+for plugin in [
+    Hardware,
+]:
+    plugin_factory.register(plugin())
