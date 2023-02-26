98179c54c63908fb295031f1664def8730976113
M. Rehan <mrehanlm93@gmail.com>
2023-02-26 14:11:38 +0500

    Allow non-serializable content for python metric

--- ixdiagnose/plugins/metrics/python.py
+++ ixdiagnose/plugins/metrics/python.py
@@ -13,13 +13,14 @@ class PythonMetric(Metric):
 
     def __init__(
         self, name: str, callback: Callable, context: Any = None, description: str = None,
-        prerequisites: List[Prerequisite] = None,
+        prerequisites: List[Prerequisite] = None, serializable: bool = True,
     ):
         super().__init__(name, prerequisites)
         self.callback: Callable = callback
         self.context: Any = context
         self.description: str = description
-        if not callable(self.context):
+        self.serializable: bool = serializable
+        if not callable(self.callback):
             raise CallError('Callback must be a callable')
         else:
             if len(inspect.signature(self.callback).parameters) == 2:
@@ -28,13 +29,17 @@ class PythonMetric(Metric):
                     'second being any context specified for the callback'
                 )
 
+    @property
+    def output_file_extension(self) -> str:
+        return '.json' if self.serializable else '.txt'
+
     def execute_impl(self) -> Tuple[Any, str]:
         report = {'error': None}
         output = None
         try:
             with get_middleware_client() as client:
-                output = self.callback(client, self.callback)
+                output = self.callback(client, self.context)
         except Exception as e:
             report['error'] = f'Failed to execute defined callback: {e!r}'
 
-        return report, report or '' if isinstance(output, str) or output is None else dumps(output)
+        return report, output or '' if isinstance(output, str) or output is None else dumps(output)
