bfb31eff16e5c0ae2a1a6c8e0b9bfe5a3ea33e2d
M. Rehan <mrehanlm93@gmail.com>
2023-03-19 13:59:58 +0500

    Make sure coredumps don't exceed 30mb in size

--- ixdiagnose/artifacts/coredumps.py
+++ ixdiagnose/artifacts/coredumps.py
@@ -1,7 +1,7 @@
 import os
 import re
 
-from typing import Optional
+from typing import Optional, Tuple
 
 from .base import Artifact
 from .items import Pattern
@@ -11,6 +11,8 @@ RE_CORE_NAME = re.compile(r'^core\.(.+?)\..+')
 
 
 class CorePattern(Pattern):
+
+    MAX_ARTIFACT_SIZE: int = 30 * 1024 * 1024
 
     def __init__(self, max_size: Optional[int] = None):
         super().__init__(r'^core\..+', max_size, truncate_files=False)
@@ -25,6 +27,23 @@ class CorePattern(Pattern):
             items[core_name] = entry
 
         return list(items.values())
+
+    def to_be_copied_checks(self, item_path: str) -> Tuple[bool, Optional[dict]]:
+        to_copy, report = super().to_be_copied_checks(item_path)
+        if not to_copy:
+            return to_copy, report
+
+        total_size = 0
+        for item in filter(lambda i: i not in self.to_skip_items, self.items):
+            item_size = item.size(item_path)
+            if total_size + item_size > self.MAX_ARTIFACT_SIZE:
+                self.to_skip_items.append(item)
+                report[item.name] = f'Skipped due to {item_size / 1024 / 1024!r} MB exceeding ' \
+                                    f'coredump artifact {self.MAX_ARTIFACT_SIZE / 1024 / 1024!r} MB max size'
+            else:
+                total_size += item_size
+
+        return len(self.items) != len(self.to_skip_items), report
 
 
 class CoreDumps(Artifact):
