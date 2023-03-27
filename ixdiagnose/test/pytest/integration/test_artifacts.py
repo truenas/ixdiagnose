e2e3637c88653f5934d669768348ff4f2db9fe4a
M. Rehan <mrehanlm93@gmail.com>
2023-03-27 10:07:40 +0500

    Validate report structure

--- ixdiagnose/test/pytest/integration/test_artifacts.py
+++ ixdiagnose/test/pytest/integration/test_artifacts.py
@@ -1,9 +1,36 @@
 import contextlib
+import json
 import os
 import shutil
 
 from ixdiagnose.artifact import artifact_factory, gather_artifacts as gather_artifacts_impl
 from ixdiagnose.config import conf
+from jsonschema import validate
+
+from .utils import BASE_REPORT_SCHEMA
+
+
+ARTIFACT_REPORT_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'execution_time': {'type': 'number'},
+        'item_report': {
+            'anyOf': [
+                {
+                    'type': 'object',
+                    'properties': {
+                        'error': {'type': ['string', 'null', 'object']},
+                        'traceback': {'type': ['string', 'null']},
+                        'copied_items': {'type': 'array'},
+                    },
+                },
+                {'type': 'null'}
+            ]
+        },
+        'item_execution_error': {'type': ['string', 'null']},
+        'item_execution_traceback': {'type': ['string', 'null']},
+    },
+}
 
 
 @contextlib.contextmanager
@@ -42,3 +69,25 @@ def test_artifacts_count():
     with gather_artifacts() as artifacts_dir:
         artifacts_dirs = get_artifacts_dirs(artifacts_dir)
         assert len(artifacts_dirs) == len(artifact_factory.get_items())
+
+
+def test_report_schema():
+    artifacts = artifact_factory.get_items()
+    with gather_artifacts() as artifacts_dir:
+        with open(os.path.join(artifacts_dir, 'report.json')) as f:
+            base_report = json.loads(f.read())
+
+        assert set(artifacts) == set(base_report)
+
+        for artifact_name, artifact_report in base_report.items():
+            validate(base_report[artifact_name], BASE_REPORT_SCHEMA)
+
+        for artifact_dir in get_artifacts_dirs(artifacts_dir):
+            with open(os.path.join(artifact_dir, 'report.json')) as f:
+                artifact_report = json.loads(f.read())
+
+            artifact = artifacts[os.path.basename(artifact_dir)]
+            assert {item.name for item in artifact.items} == set(artifact_report)
+
+            for item_report in artifact_report.values():
+                validate(item_report, ARTIFACT_REPORT_SCHEMA)
--- ixdiagnose/test/pytest/integration/test_plugins.py
+++ ixdiagnose/test/pytest/integration/test_plugins.py
@@ -1,9 +1,48 @@
 import contextlib
+import json
 import os
 import shutil
 
 from ixdiagnose.config import conf
 from ixdiagnose.plugin import generate_plugins_debug, plugin_factory
+from jsonschema import validate
+
+from .utils import BASE_REPORT_SCHEMA
+
+
+PLUGINS_REPORT_SCHEMA = {
+    'type': 'object',
+    'properties': {
+        'execution_time': {'type': 'number'},
+        'metric_report': {
+            'anyOf': [
+                {
+                    'type': 'array',
+                    'items': {
+                        'type': 'object',
+                        'properties': {
+                            'error': {'type': ['string', 'null', 'object']},
+                            'command': {'type': ['string', 'array']},
+                            'execution_time': {'type': 'number'},
+                            'description': {'type': ['string', 'null']},
+                            'returncode': {'type': 'integer'},
+                            'endpoint': {'type': 'string'},
+                        },
+                    }
+                },
+                {
+                    'type': 'object',
+                    'properties': {
+                        'error': {'type': ['string', 'null']}
+                    }
+                },
+            ],
+
+        },
+        'metric_execution_error': {'type': ['string', 'null']},
+        'metric_execution_traceback': {'type': ['string', 'null']},
+    },
+}
 
 
 @contextlib.contextmanager
@@ -42,3 +81,24 @@ def test_plugins_count():
     with generate_plugins() as plugins_dir:
         plugins_dirs = get_plugins_dirs(plugins_dir)
         assert len(plugins_dirs) == len(plugin_factory.get_items())
+
+
+def test_report_schema():
+    with generate_plugins() as plugins_dir:
+        with open(os.path.join(plugins_dir, 'report.json')) as f:
+            base_report = json.loads(f.read())
+
+        assert set(plugin_factory.get_items()) == set(base_report)
+
+        for plugin_name, plugin_report in base_report.items():
+            validate(base_report[plugin_name], BASE_REPORT_SCHEMA)
+
+        for plugin_dir in get_plugins_dirs(plugins_dir):
+            with open(os.path.join(plugin_dir, 'report.json')) as f:
+                plugin_report = json.loads(f.read())
+
+            plugin = plugin_factory.get_items()[os.path.basename(plugin_dir)]
+            assert {metric.name for metric in plugin.metrics_to_execute()} == set(plugin_report)
+
+            for metric_report in plugin_report.values():
+                validate(metric_report, PLUGINS_REPORT_SCHEMA)
