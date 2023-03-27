5138aa0656793c93cf9e0c86583eb33d362dbba5
M. Rehan <mrehanlm93@gmail.com>
2023-03-27 08:48:20 +0500

    Add events/callbacks for job progress

--- ixdiagnose/artifact.py
+++ ixdiagnose/artifact.py
@@ -2,13 +2,16 @@ import os
 import traceback
 
 from .artifacts.factory import artifact_factory
+from .event import send_event
 from .utils.formatter import dumps
 from .utils.paths import get_artifacts_base_dir
 
 
-def gather_artifacts() -> None:
+def gather_artifacts(percentage: int = 0, total_percentage: int = 100) -> None:
     os.makedirs(get_artifacts_base_dir(), exist_ok=True)
+    send_event(percentage, 'Gathering artifacts')
     artifacts_report = {}
+    artifact_percentage = total_percentage / len(artifact_factory.get_items())
     for artifact_name, artifact in artifact_factory.get_items().items():
         try:
             report = artifact.gather()
@@ -18,7 +21,13 @@ def gather_artifacts() -> None:
                 'execution_error': str(exc),
                 'execution_traceback': traceback.format_exc(),
             }
+            description = f'Error gathering artifact {artifact_name!r}'
+        else:
+            description = f'Gathered artifact {artifact_name!r}'
+
         artifacts_report[artifact_name] = report
+        percentage += artifact_percentage
+        send_event(percentage, description)
 
     with open(os.path.join(get_artifacts_base_dir(), 'report.json'), 'w') as f:
         f.write(dumps(artifacts_report))
--- ixdiagnose/plugin.py
+++ ixdiagnose/plugin.py
@@ -2,13 +2,17 @@ import concurrent.futures
 import os
 import traceback
 
+from .event import send_event
 from .plugins.factory import plugin_factory
 from .utils.formatter import dumps
 from .utils.paths import get_plugin_base_dir
 
 
-def generate_plugins_debug() -> None:
+def generate_plugins_debug(percentage: int = 0, total_percentage: int = 100) -> None:
     os.makedirs(get_plugin_base_dir(), exist_ok=True)
+    send_event(percentage, 'Gathering plugins debug information')
+
+    plugin_percentage = total_percentage / len(plugin_factory.get_items())
     with concurrent.futures.ProcessPoolExecutor(max_workers=3) as exc:
         futures = {
             exc.submit(plugin.execute): plugin_name for plugin_name, plugin in plugin_factory.get_items().items()
@@ -19,14 +23,18 @@ def generate_plugins_debug() -> None:
             try:
                 report = future.result()
             except Exception as exc:
-                # TODO: Let's add logging
                 report = {
                     'execution_time': None,
                     'execution_error': str(exc),
                     'execution_traceback': traceback.format_exc(),
                 }
+                description = f'Error gathering debug information for {plugin_name!r} plugin'
+            else:
+                description = f'Gathered debug information for {plugin_name!r} plugin'
 
             plugins_report[plugin_name] = report
+            percentage += plugin_percentage
+            send_event(percentage, description)
 
     with open(os.path.join(get_plugin_base_dir(), 'report.json'), 'w') as f:
         f.write(dumps(plugins_report))
