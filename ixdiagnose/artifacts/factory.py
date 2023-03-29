eaedea2e9a89d185bc4da18826e850d9d3704fed
M. Rehan <mrehanlm93@gmail.com>
2023-03-29 19:24:05 +0500

    Remove coredump artifact

--- ixdiagnose/artifacts/factory.py
+++ ixdiagnose/artifacts/factory.py
@@ -1,13 +1,11 @@
 from ixdiagnose.utils.factory import Factory
 
-from .coredumps import CoreDumps
 from .logs import Logs
 from .sys_info import SystemInfo
 
 
 artifact_factory = Factory()
 for artifact in [
-    CoreDumps,
     Logs,
     SystemInfo,
 ]:
