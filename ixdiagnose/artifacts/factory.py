29e34b025e7ce9a6f5ea038e05834a84f675d93d
M. Rehan <mrehanlm93@gmail.com>
2023-03-29 14:16:33 +0500

    Remove crash artifact

--- ixdiagnose/artifacts/factory.py
+++ ixdiagnose/artifacts/factory.py
@@ -1,7 +1,6 @@
 from ixdiagnose.utils.factory import Factory
 
 from .coredumps import CoreDumps
-from .crash import Crash
 from .logs import Logs
 from .sys_info import SystemInfo
 
@@ -9,7 +8,6 @@ from .sys_info import SystemInfo
 artifact_factory = Factory()
 for artifact in [
     CoreDumps,
-    Crash,
     Logs,
     SystemInfo,
 ]:
