4cc3d61b8c172975feb8457241bc2674930acddb
M. Rehan <mrehanlm93@gmail.com>
2023-04-02 15:13:15 +0500

    unit test for event callbacks

---
+++ ixdiagnose/test/pytest/unit/event/test_event.py
@@ -0,0 +1,41 @@
+import pytest
+
+from ixdiagnose.event import event_callbacks
+from ixdiagnose.exceptions import CallError
+
+
+def correct_callback(progress, test):
+    pass
+
+
+def incorrect_callback(progress):
+    pass
+
+
+def test_clear_callbacks():
+    event_callbacks.register(correct_callback)
+    assert event_callbacks.CALLBACKS == [correct_callback]
+    event_callbacks.clear()
+    assert event_callbacks.CALLBACKS == []
+
+
+@pytest.mark.parametrize('callback,should_work', [
+    (
+        correct_callback,
+        True,
+    ),
+    (
+        incorrect_callback,
+        False,
+    ),
+    (
+        'not a callable',
+        False,
+    )
+])
+def test_event_callbacks(callback, should_work):
+    if not should_work:
+        with pytest.raises(CallError):
+            event_callbacks.register(callback)
+    else:
+        assert event_callbacks.register(callback) is None
