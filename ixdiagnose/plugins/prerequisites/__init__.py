e91a4b8f5f8f09581fda4a1c67ccc080620abf88
Caleb <yocalebo@gmail.com>
2023-08-17 09:14:46 -0400

    add VMPrerequisite to prevent log spam

--- ixdiagnose/plugins/prerequisites/__init__.py
+++ ixdiagnose/plugins/prerequisites/__init__.py
@@ -1,11 +1,13 @@
 from .active_directory import ActiveDirectoryStatePrerequisite, LDAPStatePrerequisite
 from .base import Prerequisite
 from .service import ServiceRunningPrerequisite
+from .vm import VMPrerequisite
 
 
 __all__ = [
     'ActiveDirectoryStatePrerequisite',
     'LDAPStatePrerequisite',
+    'VMPrerequisite',
     'Prerequisite',
     'ServiceRunningPrerequisite',
 ]
