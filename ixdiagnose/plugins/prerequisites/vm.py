c36e9e3ed7c40efee8c6d111c7be7394b14107c8
M. Rehan <mrehanlm93@gmail.com>
2023-08-17 19:15:40 +0500

    Fix VMPrerequisite
    (cherry picked from commit ba60a432990f1fcc5f9a2c8df2a5586f3fc127b2)

--- ixdiagnose/plugins/prerequisites/vm.py
+++ ixdiagnose/plugins/prerequisites/vm.py
@@ -6,7 +6,7 @@ from .base import Prerequisite
 class VMPrerequisite(Prerequisite):
 
     def evaluate_impl(self) -> bool:
-        return MiddlewareCommand('vm.supports_virtualization').execute()
+        return MiddlewareCommand('vm.supports_virtualization').execute().output
 
     def __str__(self):
         return f'{self.cache_key!r} vm service state check'
