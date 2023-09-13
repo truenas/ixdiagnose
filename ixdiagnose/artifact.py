fff77e01d73f12c36fb367dc88103795d2ebe9da
M. Rehan <mrehanlm93@gmail.com>
2023-09-13 23:41:49 +0500

    Add CLI for ixdiagnose

--- ixdiagnose/artifact.py
+++ ixdiagnose/artifact.py
@@ -2,6 +2,7 @@ import os
 import traceback
 
 from .artifacts.factory import artifact_factory
+from .config import conf
 from .event import send_event
 from .utils.formatter import dumps
 from .utils.paths import get_artifacts_base_dir
@@ -9,10 +10,13 @@ from .utils.paths import get_artifacts_base_dir
 
 def gather_artifacts(percentage: int = 0, total_percentage: int = 100) -> None:
     os.makedirs(get_artifacts_base_dir(), exist_ok=True)
+    to_execute_artifacts = {
+        artifact_name: artifact for artifact_name, artifact in artifact_factory.get_items().items()
+        if artifact_name not in conf.exclude_artifacts
+    }
     artifacts_report = {}
-    artifact_percentage = total_percentage / len(artifact_factory.get_items())
-    for artifact_name, artifact in artifact_factory.get_items().items():
-        send_event(int(percentage + 0.5), f'Gathering artifact {artifact_name!r}')
+    artifact_percentage = total_percentage / (len(to_execute_artifacts) or 1)  # We want to handle this quietly
+    for artifact_name, artifact in to_execute_artifacts.items():
 
         try:
             report = artifact.gather()
@@ -25,6 +29,7 @@ def gather_artifacts(percentage: int = 0, total_percentage: int = 100) -> None:
 
         artifacts_report[artifact_name] = report
         percentage += artifact_percentage
+        send_event(int(percentage + 0.5), f'Gathered artifact {artifact_name!r}')
 
     with open(os.path.join(get_artifacts_base_dir(), 'report.json'), 'w') as f:
         f.write(dumps(artifacts_report))
