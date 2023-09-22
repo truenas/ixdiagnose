c942e97a64d74454f2ff46bcad7b352b48f779e6
Caleb St. John <30729806+yocalebo@users.noreply.github.com>
2023-09-22 13:57:59 -0400

    NAS-124272 / 24.04 / add procfs plugin and capture spl directory tree (#74)
    * add procfs plugin and capture spl directory tree
    
    * flake8
    
    * fix unit tests

--- ixdiagnose/artifacts/factory.py
+++ ixdiagnose/artifacts/factory.py
@@ -2,11 +2,12 @@ from ixdiagnose.utils.factory import Factory
 
 from .logs import Logs
 from .sys_info import SystemInfo
-
+from .proc import ProcFS
 
 artifact_factory = Factory()
 for artifact in [
     Logs,
     SystemInfo,
+    ProcFS,
 ]:
     artifact_factory.register(artifact())
---
+++ ixdiagnose/artifacts/proc.py
@@ -0,0 +1,9 @@
+from .base import Artifact
+from .items import Directory
+
+
+class ProcFS(Artifact):
+    base_dir = '/proc'
+    name = 'proc'
+    individual_item_max_size_limit = 10 * 1024 * 1024
+    items = [Directory('spl')]
