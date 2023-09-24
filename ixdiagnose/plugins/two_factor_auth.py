d4faa83525741fdf7e5fa54c60866e5afa631b2f
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-09-24 20:20:17 +0500

    Do not retrieve user's 2fa secrets

--- ixdiagnose/plugins/two_factor_auth.py
+++ ixdiagnose/plugins/two_factor_auth.py
@@ -1,12 +1,23 @@
-from ixdiagnose.utils.middleware import MiddlewareCommand
+from typing import Any
+
+from ixdiagnose.utils.formatter import dumps
+from ixdiagnose.utils.middleware import MiddlewareClient, MiddlewareCommand
 
 from .base import Plugin
-from .metrics import MiddlewareClientMetric
+from .metrics import MiddlewareClientMetric, PythonMetric
+
+
+def twofactor_details_of_users(client: MiddlewareClient, context: Any) -> str:
+    summary = client.call('auth.twofactor.get_users_config')
+    for user in summary:
+        user['secret_configured'] = bool(user.pop('secret'))
+
+    return dumps(summary)
 
 
 class TwoFactorAuth(Plugin):
     name = '2fa'
     metrics = [
         MiddlewareClientMetric('config', [MiddlewareCommand('auth.twofactor.config')]),
-        MiddlewareClientMetric('users', [MiddlewareCommand('auth.twofactor.get_users_config')]),
+        PythonMetric('users', twofactor_details_of_users),
     ]
