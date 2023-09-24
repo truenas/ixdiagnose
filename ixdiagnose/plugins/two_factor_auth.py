d792bce64ca5aec1908b8e66c157dd851cc6522c
Waqar Ahmed <waqarahmedjoyia@live.com>
2023-09-24 20:12:27 +0500

    Add two factor auth plugin to debug

---
+++ ixdiagnose/plugins/two_factor_auth.py
@@ -0,0 +1,11 @@
+from ixdiagnose.utils.middleware import MiddlewareCommand
+
+from .base import Plugin
+from .metrics import MiddlewareClientMetric
+
+
+class TwoFactorAuth(Plugin):
+    name = '2fa'
+    metrics = [
+        MiddlewareClientMetric('config', [MiddlewareCommand('auth.twofactor.config')]),
+    ]
