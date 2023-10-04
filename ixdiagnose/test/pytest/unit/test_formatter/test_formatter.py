9c3051eb1cfa9b8d0c73892b613debf9b8c27ea5
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-10-04 08:44:00 -0300

    Remove password key when retrieving VM devices details (#86)

--- ixdiagnose/test/pytest/unit/test_formatter/test_formatter.py
+++ ixdiagnose/test/pytest/unit/test_formatter/test_formatter.py
@@ -57,7 +57,99 @@ from ixdiagnose.utils.formatter import remove_keys
                 'domainname': 'domain2',
             },
         ],
-    )
+    ),
+    (
+        ['attributes.password'],
+        {
+            'id': 1,
+            'domainname': '',
+            'bindname': '',
+            'bindpw': '',
+            'attributes': {
+                'id': 1,
+                'password': 123
+            }
+        },
+        {
+            'id': 1,
+            'domainname': '',
+            'bindname': '',
+            'bindpw': '',
+            'attributes': {
+                'id': 1,
+            }
+        },
+    ),
+    (
+        ['devices.attributes.password'],
+        [
+          {
+            'id': 1,
+            'name': '',
+            'devices': [
+              {
+                'id': 1,
+                'attributes': {
+                  'port': 5910,
+                  'password': '1',
+                },
+              }
+            ],
+          },
+        ],
+        [
+          {
+            'id': 1,
+            'name': '',
+            'devices': [
+              {
+                'id': 1,
+                'attributes': {
+                  'port': 5910,
+                },
+              }
+            ],
+          },
+        ],
+    ),
+    (
+            ['devices.attributes.credentials.password'],
+            [
+              {
+                'id': 1,
+                'name': '',
+                'devices': [
+                  {
+                    'id': 1,
+                    'attributes': {
+                      'port': 5910,
+                      'credentials': {
+                          'name': 'test',
+                          'password': 1234
+                      },
+                    },
+                  }
+                ],
+              },
+            ],
+            [
+              {
+                'id': 1,
+                'name': '',
+                'devices': [
+                  {
+                    'id': 1,
+                    'attributes': {
+                      'port': 5910,
+                        'credentials': {
+                            'name': 'test'
+                        }
+                    },
+                  }
+                ],
+              },
+            ],
+    ),
 ])
 def test_remove_keys(keys, input_data, expected_output):
     callback = remove_keys(keys)
--- ixdiagnose/utils/formatter.py
+++ ixdiagnose/utils/formatter.py
@@ -8,15 +8,26 @@ def dumps(*args, **kwargs) -> str:
     return middleware_dumps(*args, **kwargs)
 
 
+def pop_key(output, to_find, to_remove):
+    sub_obj = get(output, to_find)
+    if isinstance(sub_obj, dict):
+        sub_obj.pop(to_remove, None)
+
+
 def remove_keys(keys: List[str]) -> Callable:
     def remove(output: Union[Dict, List]) -> Union[Dict, List]:
         if isinstance(output, dict):
             for key in keys:
                 if '.' in key:
-                    to_find, to_remove = key.rsplit('.', 1)
-                    sub_obj = get(output, to_find)
-                    if isinstance(sub_obj, dict):
-                        sub_obj.pop(to_remove, None)
+                    first_attr = get(output, key.split('.')[0])
+                    if isinstance(first_attr, list):
+                        for attr in first_attr:
+                            to_find, to_remove = key.rsplit('.', 1)
+                            to_find = to_find.split('.', 1)[1]
+                            pop_key(attr, to_find, to_remove)
+                    else:
+                        to_find, to_remove = key.rsplit('.', 1)
+                        pop_key(output, to_find, to_remove)
                 else:
                     output.pop(key, None)
         else:
