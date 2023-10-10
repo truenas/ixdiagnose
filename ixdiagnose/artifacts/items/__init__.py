9214be0cc0eff6a532e8d4a980b5e76324dfd5ad
bugclerk <40872210+bugclerk@users.noreply.github.com>
2023-10-10 09:40:31 -0300

    Add glob based item to collect artifacts (#91)

--- ixdiagnose/artifacts/items/__init__.py
+++ ixdiagnose/artifacts/items/__init__.py
@@ -1,7 +1,8 @@
 from .base import Item
 from .directory import Directory
 from .file import File
+from .glob import Glob
 from .pattern import Pattern
 
 
-__all__ = ['Directory', 'File', 'Item', 'Pattern']
+__all__ = ['Directory', 'File', 'Glob', 'Item', 'Pattern']
