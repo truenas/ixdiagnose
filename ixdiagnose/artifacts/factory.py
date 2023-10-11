80f8969851ecde1482406f0f444b8cf055fc8278
M. Rehan <mrehanlm93@gmail.com>
2023-10-11 17:31:37 +0500

    Collect sysfs modules parameters

--- ixdiagnose/artifacts/factory.py
+++ ixdiagnose/artifacts/factory.py
@@ -3,12 +3,14 @@ from ixdiagnose.utils.factory import Factory
 from .logs import Logs
 from .proc import ProcFS
 from .sys_info import SystemInfo
+from .sys_parameters import SysFSParameters
 
 
 artifact_factory = Factory()
 for artifact in [
     Logs,
     ProcFS,
+    SysFSParameters,
     SystemInfo,
 ]:
     artifact_factory.register(artifact())
