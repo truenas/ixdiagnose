d383d221b93f16b11063e8fb1fd7fd99cd3f9099
themylogin <themylogin@gmail.com>
2024-01-04 12:28:55 +0100

    Collect job logs
    (cherry picked from commit 67921b84ed38e4008f013e0497dae41c2ae711b6)

--- ixdiagnose/artifacts/logs.py
+++ ixdiagnose/artifacts/logs.py
@@ -8,6 +8,7 @@ class Logs(Artifact):
     individual_item_max_size_limit = 10 * 1024 * 1024
     items = [
         Directory('ctdb'),
+        Directory('jobs'),
         Directory('libvirt'),
         Directory('netdata'),
         Directory('openvpn'),
