24ff440db16ec46264dac64b9a07291e5b3dfb97
bugclerk <40872210+bugclerk@users.noreply.github.com>
2024-01-10 12:25:37 -0300

    NAS-126012 / 23.10.2 / Add ability to ignore files/dirs for directory item (by Qubad786) (#149)

--- ixdiagnose/artifacts/proc.py
+++ ixdiagnose/artifacts/proc.py
@@ -6,4 +6,4 @@ class ProcFS(Artifact):
     base_dir = '/proc'
     name = 'proc'
     individual_item_max_size_limit = 10 * 1024 * 1024
-    items = [Directory('spl')]
+    items = [Directory(name='spl', ignore_items=['/proc/spl/kstat/zfs/dbufs'])]
