14a747f2c3333dd9b629a11d8b5480ad8e9f581a
M. Rehan <mrehanlm93@gmail.com>
2024-01-23 17:48:50 +0500

    Add /proc/net/bonding dir to debug (#158)

--- ixdiagnose/artifacts/proc.py
+++ ixdiagnose/artifacts/proc.py
@@ -6,4 +6,7 @@ class ProcFS(Artifact):
     base_dir = '/proc'
     name = 'proc'
     individual_item_max_size_limit = 10 * 1024 * 1024
-    items = [Directory(name='spl', ignore_items=['/proc/spl/kstat/zfs/dbufs'])]
+    items = [
+        Directory('net/bonding'),
+        Directory(name='spl', ignore_items=['/proc/spl/kstat/zfs/dbufs']),
+    ]
