757f9b8c2b624c2f5ca2f9f81388a6549f6266e2
Caleb <yocalebo@gmail.com>
2024-02-14 07:16:33 -0500

    add FailoverPrerequisite
    (cherry picked from commit 731248ad899ca099f728617df30a0c228213e744)

--- ixdiagnose/plugins/prerequisites/__init__.py
+++ ixdiagnose/plugins/prerequisites/__init__.py
@@ -1,11 +1,12 @@
 from .active_directory import ActiveDirectoryStatePrerequisite, LDAPStatePrerequisite
 from .base import Prerequisite
+from .failover import FailoverPrerequisite
 from .service import ServiceRunningPrerequisite
 from .vm import VMPrerequisite
 
-
 __all__ = [
     'ActiveDirectoryStatePrerequisite',
+    'FailoverPrerequisite',
     'LDAPStatePrerequisite',
     'VMPrerequisite',
     'Prerequisite',
