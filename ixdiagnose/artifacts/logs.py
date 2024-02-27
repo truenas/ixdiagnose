764ed9e011bf9e60dca9f19d5c90c8b5792e3d3e
M. Rehan <mrehanlm93@gmail.com>
2024-02-27 05:02:15 +0500

    Make sure we capture samba log directories

--- ixdiagnose/artifacts/logs.py
+++ ixdiagnose/artifacts/logs.py
@@ -1,5 +1,5 @@
 from .base import Artifact
-from .items import Directory, File, Pattern
+from .items import DirectoryPattern, File, Pattern
 
 
 class Logs(Artifact):
@@ -7,14 +7,14 @@ class Logs(Artifact):
     name = 'logs'
     individual_item_max_size_limit = 10 * 1024 * 1024
     items = [
-        Directory('ctdb'),
-        Directory('jobs'),
-        Directory('libvirt'),
-        Directory('netdata'),
-        Directory('openvpn'),
-        Directory('pods'),
-        Directory('proftpd'),
-        Directory('samba4'),
+        DirectoryPattern('ctdb'),
+        DirectoryPattern('jobs'),
+        DirectoryPattern('libvirt'),
+        DirectoryPattern('netdata'),
+        DirectoryPattern('openvpn'),
+        DirectoryPattern('pods'),
+        DirectoryPattern('proftpd'),
+        DirectoryPattern('samba4'),
         File('auth.log'),
         File('debug'),
         File('dpkg.log'),
