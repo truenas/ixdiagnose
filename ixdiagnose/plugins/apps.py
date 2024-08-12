50208a7084f89643bba32d29d8408539d2097174
Waqar Ahmed <waqarahmedjoyia@live.com>
2024-08-12 15:14:33 +0500

    Grab used_ports and gpu choices as well in debug

--- ixdiagnose/plugins/apps.py
+++ ixdiagnose/plugins/apps.py
@@ -12,6 +12,8 @@ class Apps(Plugin):
             Command('journalctl -u docker | tail -n 1000', 'Docker logs', serializable=False),
         ]),
         MiddlewareClientMetric('apps', [MiddlewareCommand('app.query')]),
+        MiddlewareClientMetric('apps_gpu_choices', [MiddlewareCommand('app.gpu_choices')]),
+        MiddlewareClientMetric('apps_used_ports', [MiddlewareCommand('app.used_ports')]),
         MiddlewareClientMetric('catalog', [MiddlewareCommand('catalog.config')]),
         MiddlewareClientMetric('docker_config', [MiddlewareCommand('docker.config')]),
         MiddlewareClientMetric('docker_status', [MiddlewareCommand('docker.status')]),
