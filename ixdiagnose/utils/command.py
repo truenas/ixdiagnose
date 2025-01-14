5d49b76b746ff5a4188b637ebac416b950881284
themylogin <themylogin@gmail.com>
2025-01-14 11:07:24 +0100

    Revert "Exclude nvidia-related changes from `/usr` daatset diff (#208)"
    This reverts commit fe4fd3a15303f8cb023c48888e4557c56131669c.
    
    (cherry picked from commit 4329c87771c19512394a1e965bb90a943c150b41)

--- ixdiagnose/utils/command.py
+++ ixdiagnose/utils/command.py
@@ -1,5 +1,5 @@
-from collections.abc import Callable
 import subprocess
+
 from typing import Optional, Union
 
 from .run import run
@@ -10,21 +10,16 @@ class Command:
     def __init__(
         self, command: Union[str, list], description: str, serializable: bool = True,
         safe_returncodes: list = None, env: Optional[dict] = None, max_lines: Optional[int] = None,
-        postprocess: Optional[Callable[[str], str]] = None,
     ):
         self.command: Union[str, list] = command
         self.description: str = description
         self.env: Optional[dict] = env
-        self.max_lines: Optional[int] = max_lines
+        self.max_length: Optional[int] = max_lines
         self.serializable: bool = serializable
         self.safe_returncodes: list = safe_returncodes or [0]
-        self.postprocess = postprocess
 
     def execute(self) -> subprocess.CompletedProcess:
         cp = run(self.command, check=False, env=self.env)
-        if cp.returncode in self.safe_returncodes:
-            if self.postprocess:
-                cp.stdout = self.postprocess(cp.stdout)
-            if self.max_lines:
-                cp.stdout = "\n".join(cp.stdout.splitlines()[:self.max_lines])
+        if self.max_length and cp.returncode in self.safe_returncodes:
+            cp.stdout = cp.stdout.splitlines()[:self.max_length]
         return cp
