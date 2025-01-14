e98331cb4bb1ad9e53216904ff328fdc2822bfc7
themylogin <themylogin@gmail.com>
2025-01-14 11:17:27 +0100

    NAS-133548 / 25.04 / Remove modified files whitelist in `/usr` (#255)
    * Revert "remove file exclusion from ixdiagnose on enterprise systems (#218)"
    
    This reverts commit 2ec4efbb792a15fb3afd0402756d3d71c5aa4be2.
    
    * Revert "Exclude nvidia-related changes from `/usr` daatset diff (#208)"
    
    This reverts commit fe4fd3a15303f8cb023c48888e4557c56131669c.
    
    * Bring back the good part of fe4fd3a

--- ixdiagnose/utils/command.py
+++ ixdiagnose/utils/command.py
@@ -1,4 +1,3 @@
-from collections.abc import Callable
 import subprocess
 from typing import Any, Optional, Union
 
@@ -10,7 +9,6 @@ class Command:
     def __init__(
         self, command: Union[str, list], description: str, serializable: bool = True,
         safe_returncodes: list = None, env: Optional[dict] = None, max_lines: Optional[int] = None,
-        postprocess: Optional[Callable[[Any, str], str]] = None,
     ):
         self.command: Union[str, list] = command
         self.description: str = description
@@ -18,14 +16,11 @@ class Command:
         self.max_lines: Optional[int] = max_lines
         self.serializable: bool = serializable
         self.safe_returncodes: list = safe_returncodes or [0]
-        self.postprocess = postprocess
         self.execution_context: Any = None
 
     def execute(self) -> subprocess.CompletedProcess:
         cp = run(self.command, check=False, env=self.env)
         if cp.returncode in self.safe_returncodes:
-            if self.postprocess:
-                cp.stdout = self.postprocess(self.execution_context, cp.stdout)
             if self.max_lines:
                 cp.stdout = "\n".join(cp.stdout.splitlines()[:self.max_lines])
         return cp
