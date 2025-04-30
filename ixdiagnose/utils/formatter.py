330d1b0fa682de467c9595cc56ee770785ddb6a8
Brian M <brian.meagher@ixsystems.com>
2025-04-29 18:30:27 -0700

    Add redact_keys formatter, modelled on remove_keys

--- ixdiagnose/utils/formatter.py
+++ ixdiagnose/utils/formatter.py
@@ -37,3 +37,39 @@ def remove_keys(keys: List[str]) -> Callable:
 
         return output
     return remove
+
+
+def redact_str(value):
+    if isinstance(value, str):
+        return '*' * len(value)
+    return value
+
+
+def redact_key(output, to_find, to_remove):
+    sub_obj = get(output, to_find)
+    if isinstance(sub_obj, dict):
+        sub_obj[to_remove] = redact_str(sub_obj[to_remove])
+
+
+def redact_keys(keys: List[str]) -> Callable:
+    def redact(output: Union[Dict, List]) -> Union[Dict, List]:
+        if isinstance(output, dict):
+            for key in keys:
+                if '.' in key:
+                    first_attr = get(output, key.split('.')[0])
+                    if isinstance(first_attr, list):
+                        for attr in first_attr:
+                            to_find, to_remove = key.rsplit('.', 1)
+                            to_find = to_find.split('.', 1)[1]
+                            redact_key(attr, to_find, to_remove)
+                    else:
+                        to_find, to_remove = key.rsplit('.', 1)
+                        redact_key(output, to_find, to_remove)
+                else:
+                    output[key] = redact_str(output[key])
+        else:
+            for item in output:
+                redact(item)
+
+        return output
+    return redact
