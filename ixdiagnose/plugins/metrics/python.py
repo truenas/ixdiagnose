840281241ed9be3b53698982d1c17fa9ee7de475
Logan Cary <logan.cary@ixsystems.com>
2025-05-28 12:52:07 -0400

    try this

--- ixdiagnose/plugins/metrics/python.py
+++ ixdiagnose/plugins/metrics/python.py
@@ -4,23 +4,29 @@ from ixdiagnose.exceptions import CallError
 from ixdiagnose.plugins.prerequisites import Prerequisite
 from ixdiagnose.utils.formatter import dumps
 from ixdiagnose.utils.middleware import MiddlewareClient
-from typing import Any, Callable, List, Optional, Tuple
+from typing import Any, Callable, Optional, Tuple, TypeVar
 
 from .base import Metric
+
+
+_Context = TypeVar('_Context')
 
 
 class PythonMetric(Metric):
 
     def __init__(
-        self, name: str, callback: Callable, context: Any = None, description: str = None,
-        prerequisites: List[Prerequisite] = None, serializable: bool = True,
+        self, name: str, callback: Callable[[MiddlewareClient, _Context], Any], context: _Context = None,
+        description: Optional[str] = None, prerequisites: Optional[list[Prerequisite]] = None,
+        serializable: bool = True,
     ):
         super().__init__(name, prerequisites)
+
         self.callback: Callable = callback
         self.context: Any = context
-        self.description: str = description
+        self.description: str = description or ''
         self.middleware_client: Optional[MiddlewareClient] = None
         self.serializable: bool = serializable
+
         if not callable(self.callback):
             raise CallError('Callback must be a callable')
         else:
