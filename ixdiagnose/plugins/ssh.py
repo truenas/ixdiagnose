c3be6392a1deb841cfa9f6af6cb55754be875396
Logan Cary <carylog9@gmail.com>
2025-05-30 13:26:39 -0400

    redact public keys

--- ixdiagnose/plugins/ssh.py
+++ ixdiagnose/plugins/ssh.py
@@ -13,7 +13,10 @@ class SSH(Plugin):
             [MiddlewareCommand(
                 'ssh.config',
                 format_output=redact_keys([
-                    'privatekey', 'host_dsa_key', 'host_ecdsa_key', 'host_ed25519_key', 'host_key', 'host_rsa_key'
+                    'privatekey', 'host_dsa_key', 'host_dsa_key_pub', 'host_dsa_key_cert_pub', 'host_ecdsa_key',
+                    'host_ecdsa_key_pub', 'host_ecdsa_key_cert_pub', 'host_ed25519_key', 'host_ed25519_key_pub',
+                    'host_ed25519_key_cert_pub', 'host_key', 'host_key_pub', 'host_rsa_key', 'host_rsa_key_pub',
+                    'host_rsa_key_cert_pub',
                 ])
             )]
         ),
