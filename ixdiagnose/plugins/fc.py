baaae307d882113fa9096c53534c24fd2ffa53a1
Brian M <brian.meagher@ixsystems.com>
2025-06-19 11:07:57 -0700

    Add fcdump.py output to debug

--- ixdiagnose/plugins/fc.py
+++ ixdiagnose/plugins/fc.py
@@ -1,13 +1,21 @@
 from ixdiagnose.utils.middleware import MiddlewareCommand
 
 from .base import Plugin
-from .metrics import MiddlewareClientMetric
+from .metrics import CommandMetric, MiddlewareClientMetric
+from ixdiagnose.utils.command import Command
 from .prerequisites import FibreChannelPrerequisite
 
 
 class FibreChannel(Plugin):
     name = 'fc'
     metrics = [
+        CommandMetric(
+            "fcdump",
+            [
+                Command(["fcdump.py", "--format", "text"], "fcdump.py --format text", serializable=False),
+            ],
+            prerequisites=[FibreChannelPrerequisite()],
+        ),
         MiddlewareClientMetric(
             'fc_hosts', [
                 MiddlewareCommand('fc.fc_hosts'),
