baa1502525786eed8ea00a8c82e2f7c0559dae3d
Andrew Walker <awalker@ixsystems.com>
2025-07-31 08:28:02 -0600

    Rework the debug collection for directoryservices
    Directoryservices are now a new single plugin. This commit removes
    the old LDAP and AD plugins and replaces it with a single one to
    reflect backend changes.

---
+++ ixdiagnose/plugins/prerequisites/directoryservices.py
@@ -0,0 +1,26 @@
+from ixdiagnose.utils.middleware import MiddlewareCommand
+
+from .base import Prerequisite
+
+
+class ActiveDirectoryStatePrerequisite(Prerequisite):
+
+    def evaluate_impl(self) -> bool:
+        response = MiddlewareCommand('directoryservices.status').execute()
+        return (response.output or {}).get('type') != 'ACTIVEDIRECTORY'
+
+    def __str__(self):
+        return 'Active directory service state check'
+
+
+class DomainJoinedPrerequisite(Prerequisite):
+    # Check whether the directory service has some join state 
+    def evaluate_impl(self) -> bool:
+        response = MiddlewareCommand('directoryservices.status').execute()
+
+        return (response.output or {}).get('type') in ('IPA', 'ACTIVEDIRECTORY') 
+
+    def __str__(self):
+        return 'Domain joined state check'
+
+
