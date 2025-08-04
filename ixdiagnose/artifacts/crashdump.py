9752ab0bc8db759f4296ee2334b4f3b6a55c56c1
Ameer Hamza <ahamza@ixsystems.com>
2025-08-05 01:55:13 +0500

    Collect systemd pstore crash dumps in ixdiagnose
    ACPI ERST is selected as the pstore backend by default, which dumps
    kernel dmesg buffer to ACPI ERST tables that are later concatenated by
    systemd. We have ACPI ERST tables available on almost all enterprise
    platforms, and panic dumps are already logged to
    `/var/lib/systemd/pstore/` by the systemd-pstore service. This change
    integrates the crash dumps into ixdiagnose.
    
    (cherry picked from commit f8b827c917398de6a1b8459676af9222cc5dbf7f)

---
+++ ixdiagnose/artifacts/crashdump.py
@@ -0,0 +1,11 @@
+from .base import Artifact
+from .items import Glob
+
+
+class Crashdump(Artifact):
+    base_dir = '/var/lib/systemd'
+    name = 'crashdump'
+    individual_item_max_size_limit = 1 * 1024 * 1024
+    items = [
+        Glob('pstore/*/dmesg.txt'),
+    ]
