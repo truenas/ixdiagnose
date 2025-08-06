df359e7ec7006b37b18ba80396e73e11196cca1a
Ameer Hamza <ahamza@ixsystems.com>
2025-08-06 16:06:06 +0500

    Add Crashdump artifact with alphabetical ordering
    (cherry picked from commit 358b904b1946cfa8ef4781a1343bbc126e00f802)

--- ixdiagnose/artifacts/factory.py
+++ ixdiagnose/artifacts/factory.py
@@ -1,18 +1,18 @@
 from ixdiagnose.utils.factory import Factory
 
+from .crashdump import Crashdump
 from .logs import Logs
 from .proc import ProcFS
 from .sys_info import SystemInfo
 from .sys_parameters import SysFSParameters
-from .crashdump import Crashdump
 
 
 artifact_factory = Factory()
 for artifact in [
+    Crashdump,
     Logs,
     ProcFS,
     SysFSParameters,
     SystemInfo,
-    Crashdump,
 ]:
     artifact_factory.register(artifact())
