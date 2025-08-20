1d13fe4b491daddd0b056937bbd5f5c631b4c68e
Waqar Ahmed <waqarahmedjoyia@live.com>
2025-08-20 16:07:42 +0500

    Grab some more diagnostic information for docker
    This commit adds changes to grab some more additional information from docker for diagnostic purposes.
    
    (cherry picked from commit 6113d383f0fff4a36535ff55d54038ef41ecbef9)

--- ixdiagnose/plugins/apps.py
+++ ixdiagnose/plugins/apps.py
@@ -1,8 +1,52 @@
+import docker
+
 from ixdiagnose.utils.command import Command
 from ixdiagnose.utils.middleware import MiddlewareCommand
 
 from .base import Plugin
-from .metrics import CommandMetric, MiddlewareClientMetric
+from .metrics import CommandMetric, MiddlewareClientMetric, PythonMetric
+from .prerequisites import ServiceRunningPrerequisite
+
+
+def docker_container_inspect(middleware_client, context):
+    try:
+        client = docker.from_env()
+
+        # Get all containers (including stopped ones)
+        containers = client.containers.list(all=True)
+
+        if not containers:
+            return {'containers': [], 'error': None}
+
+        containers_info = []
+        for container in containers:
+            # Get container attributes
+            attrs = container.attrs
+            container_info = {
+                'name': container.name,
+                'id': container.short_id,
+                'full_id': container.id,
+                'status': container.status,
+                'restart_count': attrs.get('RestartCount', 0),
+                'started_at': attrs.get('State', {}).get('StartedAt', ''),
+                'finished_at': attrs.get('State', {}).get('FinishedAt', ''),
+                'exit_code': attrs.get('State', {}).get('ExitCode', ''),
+                'oom_killed': attrs.get('State', {}).get('OOMKilled', False),
+                'pid': attrs.get('State', {}).get('Pid', ''),
+                'image': attrs.get('Config', {}).get('Image', ''),
+                'created': attrs.get('Created', ''),
+                'restart_policy': attrs.get('HostConfig', {}).get('RestartPolicy', {}),
+            }
+            containers_info.append(container_info)
+
+    except Exception as e:
+        return {'containers': [], 'error': f'Unexpected error: {e}', 'total_containers': 0}
+    else:
+        return {
+            'containers': containers_info,
+            'total_containers': len(containers_info),
+            'error': None,
+        }
 
 
 class Apps(Plugin):
@@ -11,6 +55,21 @@ class Apps(Plugin):
         CommandMetric('docker_logs', [
             Command('journalctl -u docker | tail -n 1000', 'Docker logs', serializable=False),
         ]),
+        CommandMetric('docker_processes', [
+            Command(['docker', 'ps', '-a'], 'Docker processes (all containers)', serializable=False),
+        ], prerequisites=[ServiceRunningPrerequisite('docker')]),
+        CommandMetric('docker_images', [
+            Command(['docker', 'images', '-a'], 'Docker images (all)', serializable=False),
+        ], prerequisites=[ServiceRunningPrerequisite('docker')]),
+        CommandMetric('docker_networks', [
+            Command(['docker', 'network', 'ls'], 'Docker networks', serializable=False),
+        ], prerequisites=[ServiceRunningPrerequisite('docker')]),
+        CommandMetric('docker_volumes', [
+            Command(['docker', 'volume', 'ls'], 'Docker volumes', serializable=False),
+        ], prerequisites=[ServiceRunningPrerequisite('docker')]),
+        CommandMetric('docker_stats', [
+            Command(['docker', 'stats', '--no-stream'], 'Docker container resource usage', serializable=False),
+        ], prerequisites=[ServiceRunningPrerequisite('docker')]),
         MiddlewareClientMetric('app_images', [MiddlewareCommand('app.image.query')]),
         MiddlewareClientMetric('app_dockerhub_limit', [MiddlewareCommand('app.image.dockerhub_rate_limit')]),
         MiddlewareClientMetric('apps', [MiddlewareCommand('app.query')]),
@@ -20,4 +79,8 @@ class Apps(Plugin):
         MiddlewareClientMetric('catalog_trains', [MiddlewareCommand('catalog.trains')]),
         MiddlewareClientMetric('docker_config', [MiddlewareCommand('docker.config')]),
         MiddlewareClientMetric('docker_status', [MiddlewareCommand('docker.status')]),
+        PythonMetric(
+            'docker_container_inspect', docker_container_inspect, 'Docker container detailed inspection',
+            prerequisites=[ServiceRunningPrerequisite('docker')], serializable=True,
+        ),
     ]
