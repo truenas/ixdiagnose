4415d75e8adea4f7d0fc3dd69256b41360220f2e
bugclerk <40872210+bugclerk@users.noreply.github.com>
2025-09-11 13:04:37 -0400

    sysfs_parameters: Use relative_to for clean structure (#314)
    Use the relative_to parameter introduced in PR #305 to create cleaner
    directory structure. `sysfs_parameters/sys/module/*/parameters/*`
    becomes `sysfs_parameters/*/parameters/*`.
    
    (cherry picked from commit 5f2375841a9ca3c946e52eced6adf58f429c73d5)
    
    Co-authored-by: Ameer Hamza <ahamza@ixsystems.com>

--- ixdiagnose/artifacts/sys_parameters.py
+++ ixdiagnose/artifacts/sys_parameters.py
@@ -3,9 +3,9 @@ from .items import Glob
 
 
 class SysFSParameters(Artifact):
-    base_dir = '/sys'
+    base_dir = '/sys/module'
     name = 'sysfs_parameters'
     individual_item_max_size_limit = 10 * 1024
     items = [
-        Glob('/sys/module/*/parameters/*'),
+        Glob('*/parameters/*', relative_to='/sys/module'),
     ]
