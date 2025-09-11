daf2d5fdcfeaccb88b1e37e99ce2b5181be37243
bugclerk <40872210+bugclerk@users.noreply.github.com>
2025-09-11 13:04:11 -0400

    NAS-136934 / 25.10.0 / Collect systemd pstore crash dumps in ixdiagnose (by ixhamza) (#315)
    * Collect systemd pstore crash dumps in ixdiagnose
    
    ACPI ERST is selected as the pstore backend by default, which dumps
    kernel dmesg buffer to ACPI ERST tables that are later concatenated by
    systemd. We have ACPI ERST tables available on almost all enterprise
    platforms, and panic dumps are already logged to
    `/var/lib/systemd/pstore/` by the systemd-pstore service. This change
    integrates the crash dumps into ixdiagnose.
    
    (cherry picked from commit f8b827c917398de6a1b8459676af9222cc5dbf7f)
    
    * Add relative path support to Glob for clean structure
    
    (cherry picked from commit 5d7dcadb44a7669b13d3525688632d18b21607b0)
    
    * Add Crashdump artifact with alphabetical ordering
    
    (cherry picked from commit 358b904b1946cfa8ef4781a1343bbc126e00f802)
    
    ---------
    
    Co-authored-by: Ameer Hamza <ahamza@ixsystems.com>

--- ixdiagnose/artifacts/factory.py
+++ ixdiagnose/artifacts/factory.py
@@ -1,5 +1,6 @@
 from ixdiagnose.utils.factory import Factory
 
+from .crashdump import Crashdump
 from .logs import Logs
 from .proc import ProcFS
 from .sys_info import SystemInfo
@@ -8,6 +9,7 @@ from .sys_parameters import SysFSParameters
 
 artifact_factory = Factory()
 for artifact in [
+    Crashdump,
     Logs,
     ProcFS,
     SysFSParameters,
