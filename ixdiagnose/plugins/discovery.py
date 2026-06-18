bb4f49c76fd3c23e0cc8a5c395d80a3154236ea6
bugclerk <40872210+bugclerk@users.noreply.github.com>
2026-06-18 10:58:47 -0400

    Add missing items from debug (#359)
    * truenas-discoveryd configuration and logs
    * user logs
    * various logfiles mentioned in syslog-ng config
    
    (cherry picked from commit 3aeaad5e418f5b415b9573eb07dd32a5b05dcc9c)
    
    Co-authored-by: Andrew Walker <andrew.walker@truenas.com>

---
+++ ixdiagnose/plugins/discovery.py
@@ -0,0 +1,34 @@
+from ixdiagnose.utils.command import Command
+
+from .base import Plugin
+from .metrics import CommandMetric, FileMetric
+from .prerequisites import ServiceRunningPrerequisite
+
+
+class Discovery(Plugin):
+    name = 'discovery'
+    metrics = [
+        CommandMetric(
+            'discovery_status', [
+                Command(
+                    ['truenas-discovery-status'],
+                    'TrueNAS discovery daemon runtime status (mDNS/NetBIOS/WS-Discovery)',
+                    serializable=True,
+                ),
+            ], prerequisites=[ServiceRunningPrerequisite('truenas-discoveryd')],
+        ),
+        FileMetric(
+            'truenas-discoveryd', '/etc/truenas-discovery/truenas-discoveryd.conf', extension='.conf'
+        ),
+    ]
+    raw_metrics = [
+        CommandMetric(
+            'service_status', [
+                Command(
+                    ['systemctl', 'status', 'truenas-discoveryd'],
+                    'TrueNAS discovery service status',
+                    serializable=False, safe_returncodes=[0, 3],
+                ),
+            ],
+        ),
+    ]
